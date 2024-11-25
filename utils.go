f6767b2d2e7acf41015c3214ea826fe9eec0f424
duolok <dusan.lecic5@gmail.com>
2024-11-25 19:16:36 +0100

    add: string to bool util

---
+++ utils.go
@@ -0,0 +1,14 @@
+package main
+
+import "strings"
+
+func StringToBool(s string) bool {
+	switch strings.ToLower(s) {
+	case "true", "yes", "1":
+		return true
+	case "false", "no", "0":
+		return false
+	default:
+		return false
+	}
+}
