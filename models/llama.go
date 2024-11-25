43dcd80d861f3e1d0cb8ef414a120079fc13be86
duolok <dusan.lecic5@gmail.com>
2024-11-25 13:37:50 +0100

    add: invoke and llama body methods

--- models/llama.go
+++ models/llama.go
@@ -1,19 +1,61 @@
 package models
 
+import (
+	"context"
+	"encoding/json"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/bedrockruntime"
+)
+
 type Llama3Request struct {
 	Prompt       string  `json:"prompt"`
-	MaxGenLength string  `json:"max_gen_len,omitempty`
+	MaxGenLength int     `json:"max_gen_len,omitempty`
 	Temperature  float64 `json:"max_gen_len,omitempty`
 }
 
 type Llama3Response struct {
-    Generation string `json:"generation"`
+	Generation string `json:"generation"`
 }
 
 func (r Llama3Response) SetContent(content string) {
-    r.Generation = content
+	r.Generation = content
 }
 
 func (r Llama3Response) GetContent() string {
-    return r.Generation
+	return r.Generation
+}
+
+func (wrapper Llama) LlamaBody(prompt string) []byte {
+	body, err := json.Marshal(Llama3Request{
+		Prompt:       prompt,
+		MaxGenLength: 200,
+		Temperature:  0.5,
+	})
+
+	if err != nil {
+		log.Fatal("Failed to marshal.", err)
+	}
+
+	return body
+}
+
+func (wrapper Llama) Invoke() (string, error) {
+	output, err := wrapper.bedrock.BedrockRuntimeClient.InvokeModel(context.TODO(), &bedrockruntime.InvokeModelInput{
+		ModelId:     aws.String(Llama3modelId),
+		ContentType: aws.String("application/json"),
+		Body:        wrapper.LlamaBody(wrapper.prompt),
+	})
+
+    if err != nil {
+        ProcessError(err, Llama3modelId)
+    }
+
+    var response Llama3Response
+    if err := json.Unmarshal(output.Body, &response); err != nil {
+        log.Fatal("failed to unmarshal", err)
+    }
+
+    return response.Generation, nil
 }
