1573fae8bf1d0a27242dcb8a0fe5d0a754e5e9be
duolok <dusan.lecic5@gmail.com>
2024-11-25 13:43:34 +0100

    add: simple ws endpoint

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"log"
+	"log/slog"
+	"net/http"
+
 	"github.com/aws/aws-sdk-go-v2/service/bedrockruntime"
 	"github.com/duolok/wisp/models"
 	"github.com/gorilla/websocket"
@@ -10,9 +14,22 @@ type MLWrapper struct {
 	wrapper models.ModelWrapper
 }
 
+var websocketUpgrade = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
 func main() {
 	cfg := loadConfig()
 	brc := bedrockruntime.NewFromConfig(cfg)
 	modelWrapper := models.ModelWrapper{BedrockRuntimeClient: brc}
 	wrapper := MLWrapper{modelWrapper}
+
+
+	http.HandleFunc("/ws/model", wrapper.executeModel)
+	slog.Info("Server Listening on" , "port", "8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
