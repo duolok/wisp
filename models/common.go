5496c92c0d50a276bcf3d474142a62ca3e9de31e
duolok <dusan.lecic5@gmail.com>
2025-01-11 14:32:58 +0100

    add: streaming generic func

--- models/common.go
+++ models/common.go
@@ -1,8 +1,33 @@
 package models
 
-const (
-	llama3       = "llama3"
-	embedEnglish = "embedEnglish"
-	titanG1Lite        = "titanG1Lite"
-	Llama3modelId   = "eu.meta.llama3-2-1b-instruct-v1:0"
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/service/bedrockruntime"
 )
+
+const (
+	llama3        = "llama3"
+	embedEnglish  = "embedEnglish"
+	titanG1Lite   = "titanG1Lite"
+	Llama3modelId = "eu.meta.llama3-2-1b-instruct-v1:0"
+)
+
+func CallStreamingOutputFunction(llm string, output *bedrockruntime.InvokeModelWithResponseStreamOutput, handler StreamingOutputHandler) error {
+	switch llm {
+	case llama3:
+		err := ProcessLlamaStreamingOutput(output, handler)
+		if err != nil {
+			return err
+		}
+
+	case embedEnglish:
+		// TODO
+
+	default:
+		return fmt.Errorf("unknown llm value: %s", llm)
+
+	}
+
+	return nil
+}
