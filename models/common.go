4cd860a0e679e678533219fa7f9f0c4004fc38a4
duolok <dusan.lecic5@gmail.com>
2025-01-11 12:17:46 +0100

    fix: using correct model id

--- models/common.go
+++ models/common.go
@@ -4,5 +4,5 @@ const (
 	llama3       = "llama3"
 	embedEnglish = "embedEnglish"
 	titanG1Lite        = "titanG1Lite"
-	Llama3modelId   = "meta.llama3-70b-instruct-v1:0"
+	Llama3modelId   = "meta.llama3-2-1b-instruct-v1:0"
 )
