ce1c59ea356159fba8c827db64bae7cdd954a41b
duolok <dusan.lecic5@gmail.com>
2025-01-11 14:40:45 +0100

    add: anthropic model

---
+++ models/anthropic.go
@@ -0,0 +1,81 @@
+package models
+
+type Claude3Request struct {
+	AnthropicVersion string    `json:"anthropic_version"`
+	MaxTokens        int       `json:"max_tokens"`
+	Messages         []Message `json:"messages"`
+	Temperature      float64   `json:"temperature,omitempty"`
+	TopP             float64   `json:"top_p,omitempty"`
+	TopK             int       `json:"top_k,omitempty"`
+	StopSequences    []string  `json:"stop_sequences,omitempty"`
+	SystemPrompt     string    `json:"system,omitempty"`
+}
+
+type Content struct {
+	Type string `json:"type,omitempty"`
+	Text string `json:"text,omitempty"`
+}
+
+type Message struct {
+	Role    string    `json:"role,omitempty"`
+	Content []Content `json:"content,omitempty"`
+}
+
+type Claude3Response struct {
+	ID              string            `json:"id,omitempty"`
+	Model           string            `json:"model,omitempty"`
+	Type            string            `json:"type,omitempty"`
+	Role            string            `json:"role,omitempty"`
+	ResponseContent []ResponseContent `json:"content,omitempty"`
+	StopReason      string            `json:"stop_reason,omitempty"`
+	StopSequence    string            `json:"stop_sequence,omitempty"`
+	Usage           Usage             `json:"usage,omitempty"`
+}
+
+type ResponseContent struct {
+	Type string `json:"type,omitempty"`
+	Text string `json:"text,omitempty"`
+}
+
+type Usage struct {
+	InputTokens  int `json:"input_tokens,omitempty"`
+	OutputTokens int `json:"output_tokens,omitempty"`
+}
+
+type PartialResponse struct {
+	Type    string                 `json:"type"`
+	Message PartialResponseMessage `json:"message,omitempty"`
+	Index   int                    `json:"index,omitempty"`
+	Delta   Delta                  `json:"delta,omitempty"`
+	Usage   PartialResponseUsage   `json:"usage,omitempty"`
+}
+
+type PartialResponseMessage struct {
+	ID           string               `json:"id,omitempty"`
+	Type         string               `json:"type,omitempty"`
+	Role         string               `json:"role,omitempty"`
+	Content      []interface{}        `json:"content,omitempty"`
+	Model        string               `json:"model,omitempty"`
+	StopReason   string               `json:"stop_reason,omitempty"`
+	StopSequence interface{}          `json:"stop_sequence,omitempty"`
+	Usage        PartialResponseUsage `json:"usage,omitempty"`
+}
+
+type PartialResponseUsage struct {
+	InputTokens  int `json:"input_tokens,omitempty"`
+	OutputTokens int `json:"output_tokens,omitempty"`
+}
+
+type Delta struct {
+	Type       string `json:"type,omitempty"`
+	Text       string `json:"text,omitempty"`
+	StopReason string `json:"stop_reason,omitempty"`
+}
+
+func (r Claude3Response) SetContent(content string) {
+	r.ResponseContent[0].Text = content
+}
+func (r Claude3Response) GetContent() string {
+	return r.ResponseContent[0].Text
+}
+
