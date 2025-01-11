9ee1973c841d4a64a91c4a4ab4774096aa6b0b83
duolok <dusan.lecic5@gmail.com>
2025-01-11 14:33:47 +0100

    add: load funcs

--- models/load.go
+++ models/load.go
@@ -2,6 +2,8 @@ package models
 
 import (
 	"errors"
+
+	"github.com/aws/aws-sdk-go-v2/service/bedrockruntime"
 )
 
 func (wrapper ModelWrapper) LoadModel(modelName string, prompt string) (string, error) {
@@ -13,10 +15,10 @@ func (wrapper ModelWrapper) LoadModel(modelName string, prompt string) (string, 
 			return "", err
 		}
 		return response, nil
-    case embedEnglish:
-        // TODO
-    case titanG1Lite:
-        // TODO
+	case embedEnglish:
+		// TODO
+	case titanG1Lite:
+		// TODO
 
 	default:
 		return "", errors.New("No such model: " + modelName)
@@ -24,4 +26,23 @@ func (wrapper ModelWrapper) LoadModel(modelName string, prompt string) (string, 
 	}
 
 	return "", errors.New("No such model" + modelName)
+}
+
+func (wrapper ModelWrapper) LoadStreamingModel(modelName string, prompt string) (*bedrockruntime.InvokeModelWithResponseStreamOutput, error) {
+	switch modelName {
+	case llama3:
+		llama := Llama{LLMPrompt{wrapper, prompt}}
+		response, err := llama.Stream()
+		if err != nil {
+			return nil, err
+		}
+		return response, nil
+
+	case embedEnglish:
+			// TODO	
+	default:
+		return nil, errors.New("No such model: " + modelName)
+
+	}
+	return nil, errors.New("No such model: " + modelName)
 }
